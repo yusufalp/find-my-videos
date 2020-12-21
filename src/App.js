5e4759dcac04628d99b5d098b3719a1d74f812e0
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-21 06:51:49 -0800

    Adjusted section for selected video

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import VideoList from './components/VideoList/VideoList';
+import VideoDetail from './components/VideoDetail/VideoDetail';
+
 require('dotenv').config()
 
 class App extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   }
   searchSubmitHandler = (searchTerm) => {
     const API_KEY = `${process.env.REACT_APP_API_KEY}`;
-    let url = `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&part=snippet&chart=mostPopular`;
-   
+    let url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&maxResults=10`;
+
     fetch(`${url}&q=${searchTerm}`)
       .then(res => {
         if (!res.ok) {
@@ -23,14 +26,25 @@ class App extends React.Component {
       .then(data => this.setState({ videos: data.items }))
       .catch(err => console.log(err));
   }
+  selectVideoHandler = (video) => {
+    this.setState({ selectedVideo: video })
+  }
+
   render() {
-    console.log(this.state.videos)
+
     return (
       <div className="App">
         <h1>Find My Video</h1>
         <SearchBar onSearchSubmit={this.searchSubmitHandler} />
-        <p>I have {this.state.videos.length} videos.</p>
-        <VideoList videos={this.state.videos} />
+        <h2>Videos</h2>
+        <div className='video-container'>
+          <div className='video-list'>
+            <VideoDetail video={this.state.selectedVideo} />
+          </div>
+          <div className='video-list'>
+            <VideoList selectVideo={this.selectVideoHandler} videos={this.state.videos} />
+          </div>
+        </div>
       </div>
     );
   }
